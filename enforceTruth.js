40e8f697eb323d1610175fffe9fde3283de7d37e
Michael Noronha <michaeltnoronha@gmail.com>
2017-08-29 22:40:24 -0400

    Regex and stuff

--- enforceTruth.js
+++ enforceTruth.js
@@ -1,30 +1,24 @@
 
-// Google
-
-function enforceTruthGoogle() {
-    var query = document.getElementById('cwles');
-    var result = document.getElementById('cwos');
-    if (query.innerText.trim() === '2 + 2 =') {
-        result.innerText = 5;
+function enforceTruth(queryId,resId) {
+    var query = document.getElementById(queryId);
+    var result = document.getElementById(resId);
+    if(!query || !result) {
+        query = document.getElementsByClassName(queryId)[0];
+        result = document.getElementsByClassName(resId)[0];
     }
-
-}
-
-// Bing
-
-function enforceTruthBing() {
-    var query = document.getElementById('rcEqB');
-    var result = document.getElementById('rcTB');
-    if (query.innerText.trim() === '2+2=') {
+    if (/2\s?\+\s?2\s?=?$/.test(query.innerText)) {
         result.innerText = 5;
     }
 }
+
 if (document.title.indexOf("Google") != -1) {
-    enforceTruthGoogle();
+    enforceTruth('cwles','cwos');
 }
-if (document.title.indexOf("Bing") != -1) {
-    enforceTruthBing();
-} else {
-    console.log("nope");
+else if (document.title.indexOf("Bing") != -1) {
+    enforceTruth('rcEqB','rcTB');
+}
+else if (document.title.indexOf("DuckDuckGo") != -1) {
+    enforceTruth('expression','display');
+    enforceTruth('tile__past-formula one-line','tile__past-result one-line');
 }
 
