c3aa10b4a25f1746bcc421c296ceecf9d4e95b2c
Michael Noronha <michaeltnoronha@gmail.com>
2017-08-29 21:58:46 -0400

    It has been done

--- enforceTruth.js
+++ enforceTruth.js
@@ -1,7 +1,6 @@
-function enforceTruth() {
-    var query = document.getElementById('cwles');
-    var result = document.getElementById('cwos');
-    if (query.innerText.trim() === '2 + 2 =') {
-        result.innerText = 5;
-    }
+console.log('this was invoked');
+var query = document.getElementById('cwles');
+var result = document.getElementById('cwos');
+if (query.innerText.trim() === '2 + 2 =') {
+    result.innerText = 5;
 }
