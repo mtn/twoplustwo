581efe74fd18c7c7369d0b11432966b256665542
Brendan Whiting <brendan.whiting@me.com>
2017-08-29 21:59:54 -0400

    +bing

--- enforceTruth.js
+++ enforceTruth.js
@@ -1,7 +1,21 @@
-function enforceTruth() {
+// Google
+
+function enforceTruthGoogle() {
     var query = document.getElementById('cwles');
     var result = document.getElementById('cwos');
     if (query.innerText.trim() === '2 + 2 =') {
         result.innerText = 5;
     }
 }
+
+// Bing
+
+function enforceTruthBing() {
+    var query = document.getElementById('rcEqB');
+    var result = document.getElementById('rcTB');
+    if (query.innerText.trim() === '2+2=') {
+        result.innerText = 5;
+    }
+}
+enforceTruthGoogle();
+enforceTruthBing();
